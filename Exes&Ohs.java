fb2f60b14523e6ed20e723960970b5e6165a4b5f
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-06-18 09:56:57 +0200

    added new kata

---
+++ Exes&Ohs.java
@@ -0,0 +1,38 @@
+// DESCRIPTION:
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+// SOLUTION
+
+
+
+public class XO {
+  
+  public static boolean getXO (String str) {
+    
+    String lowerCaseStr = str.toLowerCase();
+        int countX = 0;
+        int countO = 0;
+
+        for (int i = 0; i < lowerCaseStr.length(); i++) {
+            char c = lowerCaseStr.charAt(i);
+            if (c == 'x') {
+                countX++;
+            } else if (c == 'o') {
+                countO++;
+            }
+        }
+
+        return countX == countO;
+    
+  }
+}
